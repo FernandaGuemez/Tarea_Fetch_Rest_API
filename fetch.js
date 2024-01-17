86efe7dc4f9d0e4f1b1bedd82a907c70d9d33285
FernandaG <fernanda.guemez@gmail.com>
2024-01-17 14:20:49 -0600

    Se agregan modificaciones del archivo

---
+++ fetch.js
@@ -0,0 +1,106 @@
+// Fetch
+//
+// POST
+
+const BASE_URL = "https://pokeapi.co/api/v2/";
+
+// Fetch no async
+/*
+fetch(BASE_URL + 'pokemon/ditto')
+    .then(res => res.json())
+    .then(data => console.log(data));
+*/
+// fetch async
+
+const fetchPokemon = async (pokemon) => {
+  try {
+    const response = await fetch(`${BASE_URL}pokemon/${pokemon}`);
+    const parsedResponse = await response.json();
+    return parsedResponse;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Obtener pokemon
+document.getElementById("get-btn").addEventListener("click", async () => {
+  const text = document.getElementById("poke-name").value.toLowerCase();
+  const pokemon = await fetchPokemon(text);
+  localStorage.setItem("currentPokeId", pokemon.id);
+  console.log("Nombre pokemon: " + pokemon.name);
+  console.log("Peso pokemon: " + pokemon.weight);
+  console.log("ID: " + pokemon.id);
+  console.log(pokemon.sprites.front_default);
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const storedId = localStorage.getItem("currentPokeId");
+  const initialId = storedId ? parseInt(storedId) : 1;
+  const pokemon = await fetchPokemon(initialId);
+  console.log("Nombre pokemon: " + pokemon.name);
+  console.log("Peso pokemon: " + pokemon.weight);
+  console.log("ID: " + pokemon.id);
+  console.log(pokemon.sprites.front_default);
+});
+
+// obtener el anterior
+//
+//
+
+document.getElementById("previous-btn").addEventListener("click", async () => {
+  const currentPokeId = parseInt(localStorage.getItem("currentPokeId"));
+  const newId = Math.max(1, currentPokeId - 1);
+  const pokemon = await fetchPokemon(newId);
+  console.log(pokemon);
+  console.log("Nombre pokemon: " + pokemon.name);
+  console.log("Peso pokemon: " + pokemon.weight);
+  console.log("ID: " + pokemon.id);
+  console.log(pokemon.sprites.front_default);
+  localStorage.setItem("currentPokeId", pokemon.id);
+});
+
+// obtener el siguiente
+
+document.getElementById("next-btn").addEventListener("click", async () => {
+  const currentPokeId = parseInt(localStorage.getItem("currentPokeId"));
+  const newId = currentPokeId + 1;
+  const pokemon = await fetchPokemon(newId);
+  console.log(pokemon);
+  console.log("Nombre pokemon: " + pokemon.name);
+  console.log("Peso pokemon: " + pokemon.weight);
+  console.log("ID: " + pokemon.id);
+  console.log(pokemon.sprites.front_default);
+  localStorage.setItem("currentPokeId", pokemon.id);
+});
+
+const showInfo = (name, id, weight, img) => {
+  const nombreCarta = document.getElementById("Carta_nombre");
+  const idCarta = document.getElementById("Carta_id");
+  const pesoPokemon = document.getElementById("Carta_peso");
+  const imagenCarta = document.getElementById("Carta_imagen");
+  nombreCarta.innerHTML = name;
+  idCarta.innerHTML = id;
+};
+
+////////////////// POST
+//Este es un ejemplo de fetch usando API: json placeholder:
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         title: 'title1',
+//         body: 'Lorem ipsum dolor sit amet',
+//         userId: 1,
+//     }),
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//     }
+// }).then(res => res.json())
+//     .then(json => console.log(json))
+
+/////////////////// EJERCICIOS
+//- Arreglar el pokemon en localStorage
+// - Manipular el DOM y agregar una tarjeta del pokemon.
+// - El tamaño e info de la tarjeta es a consideración personal.
+// - La tarjeta debe mantenerse en la pantalla.
+// - La info -> LocalStorage -> Fetch
